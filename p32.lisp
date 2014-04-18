37a0a14043be4fa3ebc50dc35990ae381e8a7a94
Mike Appleby <mike@peacecorps.org.cv>
2014-04-18 11:05:13 -0500

    Solution for p32.

---
+++ p32.lisp
@@ -0,0 +1,8 @@
+;;;; (**) Determine the greatest common divisor of two positive integer numbers.
+;;;; Use Euclid's algorithm.
+;;;; Example:
+;;;; * (gcd 36 63)
+;;;; 9
+
+(defun p32-gcd (n m)
+  (gcd n m))
