4eb35698bf3a9bbe0fcf856495636f129143287c
Mike Appleby <mike@peacecorps.org.cv>
2014-04-18 11:07:40 -0500

    Solution for p33.

---
+++ p33.lisp
@@ -0,0 +1,8 @@
+;;;; (*) Determine whether two positive integer numbers are coprime.
+;;;; Two numbers are coprime if their greatest common divisor equals 1.
+;;;; Example:
+;;;; * (coprime 35 64)
+;;;; T
+
+(defun p33-coprime (m n)
+  (= (gcd m n) 1))
