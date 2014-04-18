add4ab31f2200a331ba0afeaf27b68541c1af20c
Mike Appleby <mike@peacecorps.org.cv>
2014-04-18 11:16:36 -0500

    Solution for p34.

---
+++ p34.lisp
@@ -0,0 +1,26 @@
+;;;; (**) Calculate Euler's totient function phi(m).
+;;;;
+;;;; Euler's so-called totient function phi(m) is defined as the
+;;;; number of positive integers r (1 <= r < m) that are coprime to m.
+;;;;
+;;;; Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special
+;;;; case: phi(1) = 1.
+;;;;
+;;;; * (totient-phi 10)
+;;;; 4
+;;;;
+;;;; Find out what the value of phi(m) is if m is a prime
+;;;; number. Euler's totient function plays an important role in one
+;;;; of the most widely used public key cryptography methods (RSA). In
+;;;; this exercise you should use the most primitive method to
+;;;; calculate this function (there are smarter ways that we shall
+;;;; discuss later).
+
+(load "p33.lisp")
+
+(defun p34-totient-phi (m)
+  (when (< m 1)
+    (error "~a is not positive" m))
+  (if (= m 1)
+      1
+      (loop for i from 1 below m count (p33-coprime m i))))
