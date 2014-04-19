fc7813f96388b29d69f48564ebc8530f8f22fb2e
Mike Appleby <mike@peacecorps.org.cv>
2014-04-19 14:21:10 -0500

    Fix is-prime to work with inputs < 2.

--- p31.lisp
+++ p31.lisp
@@ -6,13 +6,15 @@
 (require :lisp-unit)
 
 (defun p31-is-prime (n)
-  (loop for i upfrom 2 until (> i (sqrt n))
-     never (= 0 (rem n i))))
+  (if (< n 2)
+      nil
+      (loop for i upfrom 2 until (> i (sqrt n))
+	 never (= 0 (rem n i)))))
 
 (lisp-unit:define-test is-prime-with-primes
   (loop for prime in '(2 3 5 7 11 23 104729 611953)
      do (lisp-unit:assert-true (p31-is-prime prime))))
 
-(lisp-unit:define-test is-prime-with-composites
-  (loop for composite in '(4 6 8 10 187 104730 611954)
-     do (lisp-unit:assert-false (p31-is-prime composite))))
+(lisp-unit:define-test is-prime-with-non-primes
+  (loop for non-prime in '(0 1 4 6 8 10 187 104730 611954)
+     do (lisp-unit:assert-false (p31-is-prime non-prime))))
