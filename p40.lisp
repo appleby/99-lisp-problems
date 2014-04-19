e8c5469dbda7baf823925e1fd19905fe5d703e02
Mike Appleby <mike@peacecorps.org.cv>
2014-04-19 15:24:06 -0500

    Solution for p40.

---
+++ p40.lisp
@@ -0,0 +1,37 @@
+;;;; (**) Goldbach's conjecture.
+;;;;
+;;;;
+;;;; Goldbach's conjecture says that every positive even number
+;;;; greater than 2 is the sum of two prime numbers. Example: 28 = 5 +
+;;;; 23. It is one of the most famous facts in number theory that has
+;;;; not been proved to be correct in the general case. It has been
+;;;; numerically confirmed up to very large numbers (much larger than
+;;;; we can go with our Prolog system). Write a predicate to find the
+;;;; two prime numbers that sum up to a given even integer.
+;;;;
+;;;; Example:
+;;;; * (goldbach 28)
+;;;; (5 23)
+
+(require :lisp-unit)
+
+(load "p31.lisp")
+(load "p39.lisp")
+
+(defun p40-goldbach (n)
+  (let* ((primes (p39-prime-range 2 (- n 2)))
+	 (reverse-primes (reverse primes)))
+    (loop for p1 in primes do
+	 (loop for p2 in reverse-primes
+	    for sum = (+ p1 p2)
+	    until (or (< sum n) (< p2 p1))
+	    when (= sum n) do (return-from p40-goldbach (list p1 p2))))))
+
+(lisp-unit:define-test p40-goldbach-known-inputs
+  (lisp-unit:assert-equal '(5 23) (p40-goldbach 28)))
+
+(lisp-unit:define-test p40-goldbach-test
+  (loop for i from 4 upto 1000 by 2
+     do (let ((result (p40-goldbach i)))
+	  (lisp-unit:assert-true (every #'p31-is-prime result))
+	  (lisp-unit:assert-eql i (apply #'+ result)))))
