8a6af1d50a8318b270da560c63773a33c1081662
Mike Appleby <mike@peacecorps.org.cv>
2014-04-25 17:55:38 -0500

    Solution for p55.

---
+++ p55.lisp
@@ -0,0 +1,48 @@
+;;;; (**) Construct completely balanced binary trees
+;;;;
+;;;; In a completely balanced binary tree, the following property
+;;;; holds for every node: The number of nodes in its left subtree and
+;;;; the number of nodes in its right subtree are almost equal, which
+;;;; means their difference is not greater than one.
+;;;;
+;;;; Write a function cbal-tree to construct completely balanced
+;;;; binary trees for a given number of nodes. The function should
+;;;; generate all solutions. Put the symbol 'x' as information into
+;;;; all nodes of the tree.
+;;;;
+;;;; Example:
+;;;; * (cbal-tree-print 4)
+;;;; (X (X NIL NIL) (X NIL (X NIL NIL)))
+;;;; (X (X NIL NIL) (X (X NIL NIL) NIL))
+;;;; etc......
+;;;; Note: you can either print the trees or return a list with them all.
+;;;;
+;;;; * (cbal-tree 4)
+;;;; ((X (X NIL NIL) (X NIL (X NIL NIL))) (X (X NIL NIL) (X (X NIL NIL) NIL)) ......)
+
+(in-package :99)
+
+(defun p55-unique-pairs (trees1 trees2)
+  (flet ((pair-tree-equal (pair1 pair2)
+	   (and (tree-equal (car pair1) (car pair2))
+		(tree-equal (cdr pair1) (cdr pair2))))
+	 (make-pairs (list1 list2)
+	   (loop for x in list1 append
+		(loop for y in list2 collect (list x y)))))
+    (union (make-pairs trees1 trees2)
+	   (make-pairs trees2 trees1)
+	   :test #'pair-tree-equal)))
+
+;;; Should memoize.
+(defun p55-cbal-tree (n)
+  (if (= n 0)
+      (list nil)
+      (let* ((n1 (truncate (1- n) 2))
+	     (n2 (- n 1 n1))
+	     (subtree-1 (p55-cbal-tree n1)))
+	(let ((left-right-tree-pairs
+	       (if (= n1 n2)
+		   (n-tuples 2 subtree-1)
+		   (p55-unique-pairs subtree-1 (p55-cbal-tree n2)))))
+	  (loop for (left-tree right-tree) in left-right-tree-pairs
+	     collect (list 'x left-tree right-tree))))))
