d60482e37a7f079a639e604ac4d9a37523048845
Mike Appleby <mike@peacecorps.org.cv>
2014-04-26 21:05:25 -0500

    Collapse nested LETs.

--- p55.lisp
+++ p55.lisp
@@ -36,13 +36,13 @@
       (list nil)
       (let* ((n1 (truncate (1- n) 2))
 	     (n2 (- n 1 n1))
-	     (subtree-1 (p55-cbal-tree n1)))
-	(let ((left-right-tree-pairs
-	       (if (= n1 n2)
-		   (cartesian-product subtree-1 subtree-1)
-		   (p55-combine-solutions subtree-1 (p55-cbal-tree n2)))))
-	  (loop for (left-tree right-tree) in left-right-tree-pairs
-	     collect (list 'x left-tree right-tree))))))
+	     (subtree-1 (p55-cbal-tree n1))
+	     (left-right-tree-pairs
+	      (if (= n1 n2)
+		  (cartesian-product subtree-1 subtree-1)
+		  (p55-combine-solutions subtree-1 (p55-cbal-tree n2)))))
+	(loop for (left-tree right-tree) in left-right-tree-pairs
+	   collect (list 'x left-tree right-tree)))))
 
 
 (define-test p55-cbal-tree
