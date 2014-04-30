43d0db8c529a54235e28ef087a74abd42c8a9834
Mike Appleby <mike@peacecorps.org.cv>
2014-04-30 17:23:33 -0500

    Solution for p61a.

---
+++ p61a.lisp
@@ -0,0 +1,25 @@
+;;;; (*) Collect the leaves of a binary tree in a list
+;;;;
+;;;; A leaf is a node with no successors. Write a function leaves to
+;;;; return them in a list.
+;;;;
+;;;; (leaves tree) returns the list of all leaves of the binary tree
+;;;; tree
+(in-package :99)
+
+(defun leaves (tree)
+  (cond ((null tree) *the-empty-tree*)
+	((and (null (tree-left tree))
+	      (null (tree-right tree)))
+	 (list tree))
+	(t (append (leaves (tree-left tree))
+		   (leaves (tree-right tree))))))
+
+(define-test leaves-test
+  (let ((inputs `((((d nil nil) (e nil nil) (g nil nil))
+		   ,*t1*)
+		  (((a nil nil))
+		   ,*t2*)
+		  (,*the-empty-tree* ,*t3*))))
+    (loop for (expected tree) in inputs
+       do (assert-true (tree-solutions-valid expected (leaves tree))))))
