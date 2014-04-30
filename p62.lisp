f7ca7d865f6db6a46375b765c54bcc367f861e10
Mike Appleby <mike@peacecorps.org.cv>
2014-04-30 17:29:30 -0500

    Solution for p62.

---
+++ p62.lisp
@@ -0,0 +1,17 @@
+;;;; (*) Collect the internal nodes of a binary tree in a list
+;;;;
+;;;; An internal node of a binary tree has either one or two non-empty
+;;;; successors. Write a function internals to collect them in a list.
+;;;;
+;;;; (internals tree) returns the list of internal nodes of the binary
+;;;; tree tree.
+(in-package :99)
+
+(defun internals (tree)
+  (cond ((null tree)
+	 *the-empty-tree*)
+	((and (null (tree-left tree))
+	      (null (tree-right tree)))
+	 *the-empty-tree*)
+	(t (cons tree (append (internals (tree-left tree))
+			      (internals (tree-right tree)))))))
