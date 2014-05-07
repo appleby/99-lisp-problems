5a03231ba98ceed7e4919dd85a1dcd9aa41d6fbc
Mike Appleby <mike@peacecorps.org.cv>
2014-05-07 10:48:18 -0500

    Solution for p70b.

---
+++ p70b.lisp
@@ -0,0 +1,17 @@
+;;;; (*) Check whether a given expression represents a multiway tree
+;;;;
+;;;; Write a function istree which succeeds if and only if its
+;;;; argument is a Lisp expression representing a multiway tree.
+;;;;
+;;;; Example:
+;;;; * (istree '(a (f g) c (b d e)))
+;;;; T
+(in-package :99)
+
+(defun is-mw-tree (list)
+  (cond ((null list) nil)
+	((symbolp list) t)
+	((listp list)
+	 (and (symbolp (car list))
+	      (loop for child in (cdr list) always (is-mw-tree child))))
+	(t nil)))
