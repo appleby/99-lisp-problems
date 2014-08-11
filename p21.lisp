6292cd0be2198b5bc4b88a1e2443af80c19a738c
Mike Appleby <mike@app.leby.org>
2014-08-11 15:35:13 -0500

    Update INSERT-AT work even if lst is null or if n > (length lst).

--- p21.lisp
+++ p21.lisp
@@ -5,12 +5,17 @@
 (in-package :99)
 
 (defun insert-at (new-elem lst n)
-  (loop for elem in lst
-        for idx upfrom 1
-        if (= idx n) collect new-elem and collect elem
-        else collect elem))
+  (cond ((= n 1)
+	 (cons new-elem lst))
+	((> n (length lst))
+	 (append lst (list new-elem)))
+	(t (loop for elem in lst
+	      for idx upfrom 1
+	      if (= idx n) collect new-elem and collect elem
+	      else collect elem))))
 
 (define-test insert-at-test
-  (assert-equal '() (insert-at 'foo '() 1))
+  (assert-equal '(foo) (insert-at 'foo '() 1))
   (assert-equal '(foo a) (insert-at 'foo '(a) 1))
+  (assert-equal '(a b c foo) (insert-at 'foo '(a b c) 10))
   (assert-equal '(a alfa b c d) (insert-at 'alfa '(a b c d) 2)))
