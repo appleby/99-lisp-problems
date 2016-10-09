e531b58221b369e6e42804e9f108f4dd68af785c
Mike Appleby <mike@app.leby.org>
2016-10-09 14:06:20 -0500

    Make my-length tail recursive.

--- p04.lisp
+++ p04.lisp
@@ -2,9 +2,12 @@
 (in-package :99-problems)
 
 (defun my-length (lst)
+  (my-length-iter lst 0))
+
+(defun my-length-iter (lst n)
   (if (null lst)
-      0
-      (1+ (my-length (cdr lst)))))
+      n
+      (my-length-iter (cdr lst) (1+ n))))
 
 (define-test my-length-test
   (assert-eq 0 (my-length '()))
