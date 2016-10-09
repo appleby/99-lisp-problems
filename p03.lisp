4f566b087398c4e7e01268de79c22ada95c7a2b1
Mike Appleby <mike@app.leby.org>
2016-10-09 14:02:07 -0500

    Don't be cheeky with element-at.

--- p03.lisp
+++ p03.lisp
@@ -6,7 +6,10 @@
 (in-package :99-problems)
 
 (defun element-at (lst n)
-  (nth (1- n) lst))
+  (assert (plusp n))
+  (cond ((null lst) nil)
+	((= 1 n) (car lst))
+	(t (element-at (cdr lst) (1- n)))))
 
 (define-test element-at-test
   (assert-eq 'c (element-at '(a b c d e) 3)))
